4c376e754c4336300207d6c3002949396136bd07
git-gabri <eleirbagm@gmail.com>
2021-06-03 17:58:06 +0200

    Second commit

--- src/ires/corso/Main.java
+++ src/ires/corso/Main.java
@@ -7,20 +7,30 @@ public class Main {
     public static void main(String[] args) {
         Scanner in;
         int max = 0;
+        int ordine = 0;
         in = new Scanner(System.in);
         System.out.println("Inserisci un numero");
         int n1 = in.nextInt();
         System.out.println("Inserisci un secondo numero");
         int n2 = in.nextInt();
-        if(n1>n2)
+        if(n1>n2) {
             max = n1;
-        else
+            ordine = 1;
+        }
+        else {
             max = n2;
+            ordine = 2;
+        }
+
         System.out.println("Inserisci un terzo numero");
         int n3 = in.nextInt();
+        if(n1==n2 && n1==n3 && n2==n3)
+            System.out.println("I tre numeri sono uguali");
         if(max>n3)
-            System.out.println("Il maggiore è : " + max);
-        else
-            System.out.println("Il maggiore è : " + n3);
+            System.out.println("Il maggiore è : " + max + " ed è il : " + ordine);
+         if(max<n3){
+            ordine = 3;
+            System.out.println("Il maggiore è : " + n3 + " ed è il : " + ordine);
+        }
     }
 }
